f7b780eb417a13170044f45bedd811328401b5ab
TechnoDot <techdudie@mail.com>
2025-04-02 09:24:46 -0500

    i give up

--- src/main.rs
+++ src/main.rs
@@ -24,39 +24,6 @@ fn is_valid_target(target: &str) -> bool {
     return re.is_match(target)
 }
 
-async fn run_api(state: ProxyState) {
-    let state_filter = warp::any().map(move || state.clone());
-
-    let set_target = warp::path!("set_proxy" / String)
-        .and(state_filter)
-        .and_then(|new_target: String, state: ProxyState| async move {
-            if !is_valid_target(&new_target) {
-                log(
-                    &format!(
-                        "Received command to switch to invalid server address: {}",
-                        new_target
-                    ),
-                    "WARN",
-                );
-                return Ok::<_, Infallible>(warp::reply::with_status(
-                    "Invalid address".to_string(),
-                    warp::http::StatusCode::BAD_REQUEST,
-                ));
-            }
-
-            let mut addr = state.target_addr.lock().unwrap();
-            *addr = new_target.clone();
-            log(&format!("Proxy server set to: {}", new_target), "INFO");
-
-            Ok::<_, Infallible>(warp::reply::with_status(
-                "Proxy updated".to_string(),
-                warp::http::StatusCode::OK,
-            ))
-        });
-
-    warp::serve(set_target).run(([127, 0, 0, 1], 7070)).await;
-}
-
 async fn handle_proxy(client_stream: &mut TcpStream) -> std::io::Result<TcpStream> {
     let mut buf = [0u8; 1024];
 
@@ -103,17 +70,11 @@ async fn handle_proxy(client_stream: &mut TcpStream) -> std::io::Result<TcpStrea
     Ok(server_stream)
 }
 
-async fn handle_connection(client_stream: &mut TcpStream, state: ProxyState) -> std::io::Result<()> {
-    let mut buf = [0u8; 1024];
-    client_stream.read(&mut buf).await?;
+async fn handle_connection(mut client_stream: TcpStream, _state: ProxyState) -> std::io::Result<()> {
+    // Perform SOCKS5 handshake and obtain the destination server stream
+    let mut server_stream = handle_proxy(&mut client_stream).await?;
 
-    let target_addr = {
-        let addr = state.target_addr.lock().unwrap();
-        addr.clone()
-    };
-
-    let mut server_stream = handle_proxy(client_stream).await?;
-    log(&format!("Redirecting to: {}", target_addr), "INFO");
+    log("SOCKS5 connection established", "INFO");
 
     let (mut client_read, mut client_write) = client_stream.split();
     let (mut server_read, mut server_write) = server_stream.split();
@@ -123,7 +84,7 @@ async fn handle_connection(client_stream: &mut TcpStream, state: ProxyState) -> 
         copy(&mut server_read, &mut client_write)
     )?;
 
-    return Ok(())
+    Ok(())
 }
 
 async fn run_proxy(state: ProxyState) -> std::io::Result<()> {
@@ -131,15 +92,48 @@ async fn run_proxy(state: ProxyState) -> std::io::Result<()> {
     log("SOCKS5 Proxy running on 127.0.0.1:6969", "INFO");
 
     loop {
-        let (mut client_stream, _) = listener.accept().await?;
+        let (client_stream, _) = listener.accept().await?;
         let state = state.clone();
 
         tokio::spawn(async move {
-            if let Err(e) = handle_connection(&mut client_stream, state).await {
+            if let Err(e) = handle_connection(client_stream, state).await {
                 log(&format!("Error handling connection: {}", e), "ERROR");
             }
         });
     }
+}
+
+async fn run_api(state: ProxyState) {
+    let state_filter = warp::any().map(move || state.clone());
+
+    let set_target = warp::path!("set_proxy" / String)
+        .and(state_filter)
+        .and_then(|new_target: String, state: ProxyState| async move {
+            if !is_valid_target(&new_target) {
+                log(
+                    &format!(
+                        "Received command to switch to invalid server address: {}",
+                        new_target
+                    ),
+                    "WARN",
+                );
+                return Ok::<_, Infallible>(warp::reply::with_status(
+                    "Invalid address".to_string(),
+                    warp::http::StatusCode::BAD_REQUEST,
+                ));
+            }
+
+            let mut addr = state.target_addr.lock().unwrap();
+            *addr = new_target.clone();
+            log(&format!("Proxy server set to: {}", new_target), "INFO");
+
+            Ok::<_, Infallible>(warp::reply::with_status(
+                "Proxy updated".to_string(),
+                warp::http::StatusCode::OK,
+            ))
+        });
+
+    warp::serve(set_target).run(([127, 0, 0, 1], 7070)).await;
 }
 
 #[tokio::main]
